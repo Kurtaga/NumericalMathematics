36beae12a7f6a4adf891189dc3c5e11474a3fcae
Kurtaga <ak84795@student.uni-lj.si>
2025-08-26 03:12:02 +0200

    Dn02 report written

--- Dn02/src/taylor.jl
+++ Dn02/src/taylor.jl
@@ -1,11 +1,21 @@
 # Taylor expansion of erf(x) around 0
-function erf_taylor(x::Float64; terms::Int)
+function erf_taylor_slow(x::Float64; terms::Int)
     result = 0.0
     for n in 0:(terms-1)
-        term = ((-1)^n / (factorial(big(n)) * (2n + 1))) * (x^(2n + 1))
+        term = ((-1)^n / (factorial(n) * (2n + 1))) * (x^(2n + 1))
         result += term
     end
     return (2 / sqrt(pi)) * result
+end
+
+function erf_taylor(x::Float64; terms::Int=10)
+    result = 0.0
+    term = x
+    for n in 0:(terms-1)
+        result += term / (2n+1)
+        term *= -x^2 / (n+1)
+    end
+    return (2/sqrt(pi)) * result
 end
 
 # CDF of standard normal using erf_taylor
