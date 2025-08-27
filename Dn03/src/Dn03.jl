9561a4e1aca0f5fc67a7c739ef60282b09569aad
Kurtaga <ak84795@student.uni-lj.si>
2025-08-27 16:14:08 +0200

    Reset main to empty

--- Dn03/src/Dn03.jl
+++
@@ -1,9 +0,0 @@
-module Dn03
-
-export integrate_explicit_euler, cross3, norm3, rhs, explicit_euler_step, curve_length
-
-include("la.jl")
-include("explicit_euler.jl")
-include("utils.jl")
-
-end # module
--- Dn03/src/utils.jl
+++
@@ -1,7 +0,0 @@
-function curve_length(traj)
-    L = 0.0
-    for k in 2:length(traj)
-        L += norm3(traj[k] - traj[k-1])
-    end
-    return L
-end
